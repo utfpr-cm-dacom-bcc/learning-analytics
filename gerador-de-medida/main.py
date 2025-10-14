55c7974529d7dd59b2ad20fe29ac288ce6ca6457
Danilus04 <danilogarciaba@hotmail.com>
2025-10-14 18:13:51 -0300

    Feat: Main alterado para ler arquivo de input invés de request pro Ralph

--- gerador-de-medida/main.py
+++ gerador-de-medida/main.py
@@ -1,4 +1,5 @@
 from src.fetch_statements import fetch_statements
+from src.scripts.load_statements_from_file import load_statements_from_file
 
 from src.metricas.metrica_pontuacao import calcular_pontuacao
 from src.metricas.metrica_tempo import calcular_tempo_resposta
@@ -44,5 +45,26 @@ def main():
     print("Calculando métrica de porcentagem do curso acessada...")
     calcular_porcentagem_do_curso_acessada(arquivo_json)
 
+def main2():
+    
+    statements = load_statements_from_file("statements_1.json",folder='data/statements_adivinhacao')
+
+    print("Calculando métrica de pontuação...")
+    calcular_pontuacao(statements)
+
+   
+    print("Calculando métrica de tempo de resposta...")
+    calcular_tempo_resposta(statements)
+
+    print("Calculando métrica de tentativas por questionario...")
+    calcular_tentativas_por_questionario(statements)
+
+    print("Calculando métrica de tempo total gasto em visitas reais...")
+    calcular_tempo_total_gasto_em_visitas_reais(statements)
+
+    arquivo_json = "atividades.json"
+
+    print("Calculando métrica de porcentagem do curso acessada...")
+    calcular_porcentagem_do_curso_acessada('data/' + arquivo_json)
 if __name__ == "__main__":
-    main()
+    main2()
